3941b52cf6077db7bacc1497b28668e106dc7fd3
ayodeji <ayodejiakintobi1@gmail.com>
2024-12-23 13:42:56 +0100

    UPDATE: ASSESTS ADDED

---
+++ PUT/src/assets/index.js
@@ -0,0 +1,55 @@
+import airbnb from './airbnb.png';
+import binance from './binance.png';
+import coinbase from './coinbase.png';
+import dropbox from './dropbox.png';
+import logo from './logo.png';
+import quotes from './quotes.svg';
+import robot from './robot.png';
+import send from './Send.svg';
+import shield from './Shield.svg';
+import star from './Star.svg';
+import menu from './menu.svg';
+import close from './close.svg';
+import google from './google.svg';
+import apple from './apple.svg';
+import arrowUp from './arrow-up.svg';
+import discount from './Discount.svg';
+import facebook from './facebook.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
+import twitter from './twitter.svg';
+import react from './react.png';
+import banner from './banner.png';
+import about from './about.png';
+import youtube from './youtube.svg';
+import telegram from './telegram.png'
+import email from './email.png'
+
+export {
+	airbnb,
+	binance,
+	coinbase,
+	dropbox,
+	logo,
+	quotes,
+	robot,
+	send,
+	shield,
+	star,
+	menu,
+	close,
+	google,
+	apple,
+	arrowUp,
+	discount,
+	facebook,
+	instagram,
+	linkedin,
+	youtube,
+	telegram,
+	twitter,
+	react,
+	banner,
+	about,
+	email
+};
